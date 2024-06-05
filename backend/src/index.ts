408ca3426a5ce2a34b6d42645318359624b95df4
ehsan304 <muhammadehsan304@gmail.com>
2024-06-06 00:15:11 +0500

    complete the functionalities

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 
 dotenv.config()
-
+const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
@@ -14,6 +14,6 @@ app.use("/api/messages", messageRoutes)
 
 
 
-app.listen(5000, ()=>{
-    console.log("Server is running on port 5000")
+app.listen(PORT, ()=>{
+    console.log("Server is running on port " + PORT)
 })
